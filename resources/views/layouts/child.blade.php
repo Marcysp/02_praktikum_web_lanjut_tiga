5bf1f1aa678df972dcb05da7361cad56feee8800
Marcysp <marcyalvina@gmail.com>
2023-02-25 09:26:39 +0700

    hapus bagian ini

--- resources/views/layouts/child.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-@section('title','profil')
-@section('sidebar')
-@parent
-    <p>Sidebar halaman Profil.</p>
-@endsection
-@section('content')
-    <p>Ini adalah bagian konten. NIM - Nama</p>
-@endsection
